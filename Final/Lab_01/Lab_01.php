e4f060296a3d7ea0ae864a1d6d2ca95e416f7531
nafis3303 <nafis3303>
2025-11-03 22:34:08 +0600

    Final added

---
+++ Final/Lab_01/Lab_01.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<head>
+<title>Student Registration Form</title>
+</head>
+
+<body> 
+<h2 style="background-color: red; border-bottom: 4px solid black; padding: 10px;">  Student Registration Form</h2>
+
+<form>
+
+First Name: <input type="text"><br><br>
+
+Last Name: <input type="text"><br><br>
+
+Student ID: <input type="number"><br><br>
+
+Program: <input type="text"> <br> <br>
+
+Phone: <input type="number" ><br><br>
+Email: <input type="email" ><br><br>
+Create Password: <input type="text"><br><br>
+Confirm Password: <input type="text"><br><br>
+
+
+
+
+Semester Selection: 
+<input type="radio" name="sms" value="fall"> Fall
+<input type="radio" name="sms" value="spring"> Spring
+<input type="radio" name="sms" value="summer"> Summer
+<br><br>
+
+Request Credit: <input type="number"> <br>
+
+<h2 style="color: red; border-bottom: 4px solid red; padding: 10px;">Course Selection:</h2>
+<input type="checkbox"> DATACOMM
+<input type="checkbox"> WEBTECH
+<input type="checkbox"> C#
+<input type="checkbox"> COMPUTER GRAPHICS
+<input type="checkbox"> RM
+<BR></BR>
+Comment: <br>
+<textarea name="address" rows="4" cols="30"></textarea>
+<br><br>
+
+<input type="submit" value="Submit">
+<input type="reset" value="Reset">
+</form>
+
+</body>
+</html>
