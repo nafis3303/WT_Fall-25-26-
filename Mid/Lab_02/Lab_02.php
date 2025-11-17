294c1f61f1377843f4c716a7a838060e1c9a124d
nafis3303 <nafis3303>
2025-11-17 11:21:17 +0600

    work

---
+++ Mid/Lab_02/Lab_02.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<head>
+    <title>Student Registration</title>
+</head>
+<body> 
+    <h2>Student Registration</h2>
+
+    <form>
+
+        Full Name: <br> 
+        <input type="text"> <br><br>
+        Email: <br>
+        <input type="email"><br><br>
+        Password: <br>
+        <input type="password"><br><br>
+        Confirm Password: <br>
+        <input type="password"><br><br>
+    </form>
+
+    <div class="success"></div>
+
+    <h2>Course Registration</h2>
+    Course Name:<br>
+    <input type="checkbox"> Data Structures<br>
+
+</body>
