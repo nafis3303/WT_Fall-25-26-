9fb33deec4d4885d40951f493f344e6ab6574bb5
nafis3303 <nafis3303>
2025-12-08 13:00:39 +0600

    Function added

--- Final/Lab_01.php
+++ Final/Lab_01.php
@@ -4,3 +4,12 @@ $name = "Nafis";
 $age = 22;
 $dept = "CSE";
 $marks = array(85,75,90);
+
+function totalMarks($arr) {
+    $sum = 0;
+    foreach ($arr as $value) {
+        $sum += $value;
+    }
+    return $sum;
+}
+$total = totalMarks($marks);
