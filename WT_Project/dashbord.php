bf0b975240d0f83b71a4d2d46ac847f6a034bf8b
Amit <dattaamit571@gmail.com>
2025-12-22 21:21:56 +0600

    class

--- WT_Project/dashbord.php
+++ WT_Project/dashbord.php
@@ -6,7 +6,10 @@
 <body>
     <h1>Welcome to QuizeMaster</h1>
     <p>Start creating quizzes, take tests and track performance!</p>
-    
+    <p> This is Done by amit</p>
+    <div class="btn-grp">
+        <a href=" " class="btn">Login</a>
+        <!-- <a href=" " class="btn">Create Account</a> -->
        
     </div>
 
