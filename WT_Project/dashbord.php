ab12fd61515c2e712b245364273366afc2dfbe3f
Amit <dattaamit571@gmail.com>
2025-12-22 21:09:50 +0600

    dashbordadd

--- WT_Project/dashbord.php
+++ WT_Project/dashbord.php
@@ -7,7 +7,6 @@
     <h1>Welcome to QuizeMaster</h1>
     <p>Start creating quizzes, take tests and track performance!</p>
     <p> This is Done by amit</p>
-    <div class="btn-grp">
        
     </div>
 
