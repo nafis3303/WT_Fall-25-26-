3b136a3e033aef3835ea81500154847491e0f9f6
Amit <dattaamit571@gmail.com>
2025-12-22 21:00:04 +0600

    class

---
+++ WT_Project/dashbord.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+<title>Online Quiz System </title>
+
+</head>
+<body>
+    <h1>Welcome to QuizeMaster</h1>
+    <p>Start creating quizzes, take tests and track performance!</p>
+    <p> This is Done by amit</p>
+    <div class="btn-grp">
+       
+    </div>
+
+
+</body>
+</html>
