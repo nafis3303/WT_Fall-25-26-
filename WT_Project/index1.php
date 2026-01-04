efd46d8513d503fefa110a9f56aa4c916b5dc134
nafis3303 <nafis3303>
2026-01-04 23:18:17 +0600

    css updated for index

--- WT_Project/index1.php
+++ WT_Project/index1.php
@@ -6,24 +6,29 @@ if (isset($_SESSION['user_id'])) {
 }
 ?>
 
-<link rel="stylesheet" href="css/style.css" >
+<link rel="stylesheet" href="css/index1.css">
 
 <!DOCTYPE html>
+<html lang="en">
 
 <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Online Quiz System </title>
+
 
 </head>
 
 <body>
-    <h1>Welcome to QuizeMaster</h1>
-    <p>Start creating quizzes, take tests and track performance!</p>
-    
-    <div class="btn-grp">
-        <a href=" " class="btn">Login</a>
-        <a href=" " class="btn">Create Account</a>
-
+    <div class="container">
+        <h1>Welcome to QuizMaster</h1>
+        <p>Start creating quizzes, take tests, and track your performance!</p>
+        <div class="button-group">
+            <a href="login.php" class="btn">Login</a>
+            <a href="register.php" class="btn">Create Account</a>
+        </div>
     </div>
+
 
 
 </body>
