5820f413612f8fdb5a5f5bb1b9e5d3f47aa9b754
nafis3303 <nafis3303>
2026-01-19 13:20:08 +0600

    session added

--- WT_Project/student/view_results.php
+++ WT_Project/student/view_results.php
@@ -1,10 +1,33 @@
+<?php
+session_start();
+if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'student') {
+    header("Location: ../login.php");
+    exit();
+} 
+
+
+    $studentId = $_SESSION['user_id'];
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "quizzers";
+    
+
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 
 <head>
     <title>My Results - QuizMaster</title>
 </head>
+
 <body>
     <h1>View Results</h1>
 </body>
+
 </html>
